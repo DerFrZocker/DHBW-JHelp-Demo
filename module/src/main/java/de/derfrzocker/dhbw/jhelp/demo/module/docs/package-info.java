2847e422c18b235db605390c86ffcf9d1bc5da94
DerFrZocker <git@derfrzocker.de>
2024-03-06 10:12:38 +0100

    Add netbeans auto generated help set code

---
+++ module/src/main/java/de/derfrzocker/dhbw/jhelp/demo/module/docs/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/package-info.java to edit this template
+ */
+@HelpSetRegistration(helpSet = "module-hs.xml", position = 3468)
+package de.derfrzocker.dhbw.jhelp.demo.module.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;

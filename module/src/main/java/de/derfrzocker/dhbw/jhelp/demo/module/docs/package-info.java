558752ecdab3350e1b751940e20dc995fac64d86
DerFrZocker <git@derfrzocker.de>
2024-03-06 10:21:31 +0100

    Update dependency and package-info.java to JHelp

--- module/src/main/java/de/derfrzocker/dhbw/jhelp/demo/module/docs/package-info.java
+++ module/src/main/java/de/derfrzocker/dhbw/jhelp/demo/module/docs/package-info.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/package-info.java to edit this template
  */
-@HelpSetRegistration(helpSet = "module-hs.xml", position = 3468)
+@HelpSetRegistration(helpSet = "module-hs.xml")
 package de.derfrzocker.dhbw.jhelp.demo.module.docs;
 
-import org.netbeans.api.javahelp.HelpSetRegistration;
+import de.dhbw.mwulle.jhelp.netbeans.api.HelpSetRegistration;
